008b1469a4acbc99e0b9589a901bacf3e323024f
Sarkar <68526104+Sarkar22@users.noreply.github.com>
2024-12-07 06:33:39 +0000

    added printf

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,10 +1,16 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Emon Sarkar
-#include <iostream>
+#include <iostream>  // Compiler Directive
+#include <cstdio>
+
+/* This program prints
+    the message "hi there"
+    in the terminal output
+*/
 
 int main () {
-    // printf("Hello WOrld \n");
+    printf("Hello WOrld \n");
     std::cout << "Hi there!!" << std::endl;
 
     std::cout << std::endl << std::endl;
