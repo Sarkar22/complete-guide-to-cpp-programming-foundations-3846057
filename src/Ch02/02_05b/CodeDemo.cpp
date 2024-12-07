7fffe826ba96f53f7404f5ae2c9303694d23c56e
Sarkar <68526104+Sarkar22@users.noreply.github.com>
2024-12-07 22:37:26 +0000

    played with variables

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -4,8 +4,37 @@
 
 #include <iostream>
 
+int a, b = 5; // These are global variables
+// Global variable memories are statically managed by the compiler
+// They are allocated in the data segment of the memory
+// Once the prgogram ends, their memory is freed
+
+// Local varibles are only available inside the scope
+// After execution they are deleted
+// Called Automatic Variables, managed by the compilers
+// Allocated in the stack segment of the memory
+
 int main(){
-    
+
+    bool my_flag;
+    std::cout << "My flag is " << my_flag << std:: endl;
+    my_flag =false;
+    std::cout << "My flag is " << my_flag << std:: endl;
+    my_flag =true;
+    std::cout << "My flag is " << my_flag << std:: endl;
+
+    a = 100;
+    std::cout << "a: " << a << std:: endl;
+    std::cout << "b: " << b << std:: endl;
+
+    std::cout << "a+b: " << a+b << std:: endl;
+    std::cout << "a-b: " << a-b << std:: endl;
+    std::cout << "b-a: " << b-a << std:: endl;
+
+    unsigned int positive;
+    positive = b-a;
+    std::cout << "positive (b-a >> unsigned): " << positive << std:: endl;
+
     std::cout << std::endl << std::endl;
     return 0;
 }
