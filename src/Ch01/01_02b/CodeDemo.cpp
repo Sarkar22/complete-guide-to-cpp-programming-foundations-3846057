06290a22e265bba615a4cb17599057a3cbc8bbd3
Sarkar <68526104+Sarkar22@users.noreply.github.com>
2024-12-07 06:13:36 +0000

    written a basic line to cout text

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,12 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
-// Hello World, by Eduardo Corpeño 
+// Hello World, by Emon Sarkar
+#include <iostream>
+
+int main () {
+    // printf("Hello WOrld \n");
+    std::cout << "Hi there!!" << std::endl;
+
+    std::cout << std::endl << std::endl;
+    return 0;
+}
